fa6120333a520659317d3808ef2dbc8576f77ba9
Dong-geon Lim <ooqwe486@gmail.com>
2016-08-09 04:29:22 +0000

    relative path

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Workspace
   class Application < Rails::Application
-    config.action_controller.relative_url_root = "/map"
+    config.relative_url_root = '/map'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: redirect('home/index')
   get 'home/index' 
   get 'manage' => 'home#manage'
   post 'home/add_req'
